c43804324286cbb31d30580d242305c402a29808
king-aggor <emmanuelaggor.ea@gmail.com>
2025-05-30 15:10:09 +0000

    created middleware to use authenticationRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import morgan from "morgan";
+import authenticationRoutes from "./routes/authentication";
 
 const app = express();
 
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(
   cors({
@@ -12,4 +15,8 @@ app.use(
   })
 );
 
+app.use("/api/auth", authenticationRoutes);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).json({ error: err });
+});
 export default app;
